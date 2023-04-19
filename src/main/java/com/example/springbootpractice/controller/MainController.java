cd230db02bf47baed59a0d69cb8b591b4d340a41
jeongwoo2 <jeongwoo2@knou.ac.kr>
2023-04-19 16:02:56 +0900

    Spring Boot Practice

---
+++ src/main/java/com/example/springbootpractice/controller/MainController.java
@@ -0,0 +1,44 @@
+package com.example.springbootpractice.controller;
+
+import com.example.springbootpractice.properties.MyProperties;
+import com.example.springbootpractice.service.MyPropertyService;
+import com.example.springbootpractice.service.SortService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@RestController
+public class MainController {
+
+    private final SortService sortService;
+    private final MyPropertyService myPropertyService;
+    private final MyProperties myProperties;
+    private final ContextRefresher contextRefresher;
+
+
+    @GetMapping("/")
+    public String hello(@RequestParam List<String> list) {
+        return sortService.doSort(list).toString();
+    }
+
+    @GetMapping("/my-height")
+    public Map<String, Integer> myHeight() {
+        return Map.of(
+                "- my.height(@Value from service): ", myPropertyService.getMyHeight(),
+                "- my.height(@ConfigurationProperties): ", myProperties.getHeight()
+        );
+    }
+
+    @GetMapping("/refresh")
+    public Map<String, Set<String>> refresh() {
+        return Map.of("refreshResult", contextRefresher.refresh());
+    }
+
+}
---
+++ src/main/java/com/example/springbootpractice/service/SortService.java
@@ -0,0 +1,26 @@
+package com.example.springbootpractice.service;
+
+import com.example.springbootpractice.logic.Sort;
+import com.example.springbootpractice.properties.CustomProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SortService {
+
+    private final Sort<String> sort;
+
+
+    public SortService(@Qualifier("bubbleSort") Sort<String> sort, CustomProperties customProperties) {
+//        System.out.println("프로퍼티: " + customProperties.getDuration().toMillis() + " ms");
+//        System.out.println("구현체: " + sort.getClass().getName());
+        this.sort = sort;
+    }
+
+    public List<String> doSort(List<String> list) {
+        return sort.sort(list);
+    }
+
+}
